57a1824c09a4daa62634b278650dab7bebb0e504
Maria Mills <cxwinterx@gmail.com>
2023-10-12 18:58:09 -0400

    Add cypress testing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,7 +18,7 @@ const Nav = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link active" aria-current="page" href="#">
+            <a className="nav-link active" aria-current="page" href="/home">
               Home
             </a>
             <a className="nav-link" href="#">
