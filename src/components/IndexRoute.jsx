6c3acaa21a8ecb8cddf2dc31242ec33a95a33780
Maria Mills <cxwinterx@gmail.com>
2023-10-13 16:08:09 -0400

    Fix typo: classNameName to className

--- src/components/IndexRoute.jsx
+++ src/components/IndexRoute.jsx
@@ -1,15 +1,15 @@
 function IndexRoute() {
   return (
     <>
-      <div classNameName="container-fluid my-5">
+      <div className="container-fluid my-5">
         <div className="p-5 text-center">
           <h1 className="mb-3">Welcome Space Explorer!🚀</h1>
           <h4 className="mb-3">Explore the universe's wonders.</h4>
-          <p classNameName="mb-4">
+          <p className="mb-4">
             View a random Mars Rover or NASA's Astronomy Picture of the Day
             (APOD).
           </p>
-          <a classNameName="btn btn-dark mx-2" href="/apod">
+          <a className="btn btn-dark mx-2" href="/apod">
             Astro Picture of the Day
           </a>
           <a className="btn btn-secondary mx-2" href="/mars">
